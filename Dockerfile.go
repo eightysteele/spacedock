a92fe1d9992b15f0288c0f89da858cc078af14fa
Aaron Steele <eightysteele@gmail.com>
2024-05-29 14:39:00 -0700

    cleanup

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,18 +1,16 @@
 # syntax=docker/dockerfile:1.7-labs
 
-# Installs https://github.com/golang/go in $GO_DIR/goroot. 
+# Installs https://github.com/golang/go in $GOROOT.
 
 ARG GO_VERSION
-ARG GO_DIR
-ARG GOPATH
+ARG GOROOT
 
 ################################################################################
 FROM xdg AS go
 ################################################################################
 
 ARG GO_VERSION
-ARG GO_DIR
-ARG GOPATH
+ARG GOROOT
 
 #-------------------------------------------------------------------------------
 # build dependencies
@@ -20,7 +18,6 @@ ARG GOPATH
 
 RUN bash -x <<"EOF"
 set -eu
-rm -f /etc/apt/apt.conf.d/docker-clean
 apt-get update
 apt-get install -y --no-install-recommends \
     ca-certificates \
@@ -37,10 +34,8 @@ EOF
 
 RUN bash -x <<"EOF"
 set -eu
-mkdir ${GO_DIR} && cd ${GO_DIR} || exit 1
-mkdir ${GOPATH} || exit 1
-mkdir goroot || exit 1
+grd=$(dirname "$GOROOT")
 wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
-tar -C goroot -xzf go${GO_VERSION}.linux-amd64.tar.gz
+tar -C "$grd" -xzf go${GO_VERSION}.linux-amd64.tar.gz
 rm go${GO_VERSION}.linux-amd64.tar.gz
 EOF
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
-export PATH=${SPOK_PATH}:/opt/xdg/.config/nvm/versions/node/v20.13.1/bin:$PATH
+# setup environment
+export PATH=$__PATH:$PATH
+export SPACEMACSDIR=${__SPACEMACSDIR}
+export XDG_CONFIG_HOME=${__XDG_CONFIG_HOME}
+export DISPLAY=${__DISPLAY}
 
-git config --global --add safe.directory "*"
-
+# authenticate gh
 if ! gh auth status; then
 	gh auth login
 fi
+git config --global --add safe.directory "*"
 
+# pull the latest spacemacs config
 pushd "$SPACEMACSDIR" || exit 1
+echo "pulling $SPACEMACSDIR"
 git pull
 popd || exit 1
 
+# start emacs daemon
 if pgrep -af "^emacs.*--daemon$" >/dev/null; then
 	echo "emacs daemon is running."
 else
 	echo "starting emacs daemon..."
-	yes | emacs --debug-init --daemon
+	yes | emacs --daemon
 fi
 
+# clone repos
 CODE_DIR="/code"
 cd "$CODE_DIR" || exit 1
 if [ ! -d "$CODE_DIR/emmy" ]; then
@@ -27,33 +35,23 @@ fi
 if [ ! -d "$CODE_DIR/spacedock" ]; then
 	gh repo clone eightysteele/spacedock
 fi
-if [ ! -d "$CODE_DIR/spacedock" ]; then
+if [ ! -d "$CODE_DIR/Gen.clj" ]; then
 	gh repo clone eightysteele/Gen.clj
 fi
+if [ ! -d "$CODE_DIR/priors" ]; then
+	gh repo clone eightysteele/priors
+fi
 
-omd="$XDG_CONFIG_HOME/org-roam-data"
-omdv="$XDG_CONFIG_HOME/emacs/org-roam/"
-if [ -d "$omd" ]; then
-	echo "first run, moving $omd into the $omdv volume"
-	if [ -d "$omdv/org-roam-data" ]; then
-		pushd "$omdv/org-roam-data" || exit 1
-		git pull
-		git add .
-		git commit -m 'auto commit'
-		if ! git push origin; then
-			echo "unable to git push org-roam-data"
-			exit 1
-		fi
-		popd || exit 1
-	fi
-	pushd "$omd" || exit 1
-	if ! git pull; then
-		echo "unable to git pull org-roam-data"
-		exit 1
-	fi
+# get org repo into the volume
+org_repo="$XDG_CONFIG_HOME/org-roam-data"
+org_volume="$XDG_CONFIG_HOME/emacs/org-roam/"
+if [ -d "$org_volume/org-roam-data" ]; then
+	pushd "$org_volume/org-roam-data" || exit 1
+	git pull
 	popd || exit 1
-	rm -rf "$omdv/org-roam-data"
-	mv "$omd" "$omdv"
+elif [ -d "$org_repo" ]; then
+	echo "first run, moving $org_repo into $org_volume"
+	mv "$org_repo" "$org_volume"/
 fi
 
 /bin/bash
